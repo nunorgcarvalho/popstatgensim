1932104d93e3848a1de0802623eb3b479f02ae26
Nuno Carvalho <47678528+nunorgcarvalho@users.noreply.github.com>
2025-07-07 13:51:45 -0400

    added function for plotting HE regression (#36)

--- code/popstatgen/statgen_functions.py
+++ code/popstatgen/statgen_functions.py
@@ -7,6 +7,7 @@ The functions here contain the documentation for the arguments and return values
 
 # imports
 import numpy as np
+import matplotlib.pyplot as plt
 from typing import Tuple, Union
 
 ##########################
@@ -96,12 +97,19 @@ def get_standardized_effects(effects: np.ndarray, G_std: np.ndarray, std2allelic
             effects_output = effects * G_std # per-standardized-allele effects
         return effects_output
 
+#################################
+#### Heritability Estimation ####
+#################################
+
 def run_HE_regression(A: np.ndarray, y:np.ndarray, se: str = 'jackknife') -> Union[float, Tuple[float, float]]:
     '''
     Performs Haseman-Elston regression to estimate heritability.
     Parameters:
         A (2D array): N*N matrix of pairwise genetic relatedness coefficients. Can be ei
         y (1D array): N-length array of trait values. Should be standardized.
+    Returns:
+        h2g_HE (float): Estimated heritability from Haseman-Elston regression.
+        h2g_HE_se (float): Standard error of the heritability estimate if `se` is 'jackknife'. Otherwise, this value is not returned.
     '''
     y = (y - y.mean()) / y.std() # standardizes trait values
     N = A.shape[0]
@@ -144,3 +152,53 @@ def run_HE_regression(A: np.ndarray, y:np.ndarray, se: str = 'jackknife') -> Uni
         return h2g_HE, h2g_HE_se
     else:
         return h2g_HE
+    
+def plot_HE_regression(A: np.ndarray, y: np.ndarray, bins: int = 5) -> None:
+    '''
+    Plots the Haseman-Elston regression line and the data points.
+    Parameters:
+        A (2D array): N*N matrix of pairwise genetic relatedness coefficients.
+        y (1D array): N-length array of trait values. Should be standardized.
+        bins (int): Number of bins to use for grouping relatedness coefficients. 95% confidence interval for each bin's mean is shown. Default is 5 bins. If 0, no binning is performed.
+    '''
+    relatedness = []
+    sq_diff = []
+
+    y = (y - y.mean()) / y.std() # standardizes trait values
+    N = A.shape[0]
+    # computes the squared difference between pairwise trait values
+    # and the pairwise genetic relatedness coefficients
+    for j in range(N):
+        for k in range(N):
+            if j >= k: # since matrix is symmetrical along diagonal
+                continue
+            relatedness.append( A[j,k] )
+            sq_diff.append( (y[j] - y[k])**2 )
+    
+    relatedness = np.array(relatedness)
+    sq_diff = np.array(sq_diff)
+    # binning
+    if bins > 1:
+        # defines bin edges
+        bin_edges = np.linspace(np.min(relatedness), np.max(relatedness), bins + 1)
+        # assigns each x_val to a bin index
+        x_bin_i = np.digitize(relatedness, bin_edges) - 1 # -1 to make indices 0-based
+        x_bin_i[x_bin_i == bins] = bins - 1  # ensure the last bin is inclusive
+        # computes mean x and y for each bin
+        x_val = np.array([np.mean(relatedness[x_bin_i == i]) for i in range(bins)])
+        y_val = np.array([np.mean(sq_diff[x_bin_i == i]) for i in range(bins)])
+        # computes 95CI around each point
+        y_val_se = np.array([np.std(sq_diff[x_bin_i == i]) / np.sqrt(np.sum(x_bin_i == i)) for i in range(bins)])
+    else:
+        x_val = relatedness
+        y_val = sq_diff
+
+    # plotting
+    plt.figure(figsize=(8, 5))
+    plt.scatter(x_val, y_val, alpha=0.5)
+    if bins > 1:
+        plt.errorbar(x_val, y_val, yerr=1.96*y_val_se, fmt='o', color='black', capsize=5, label='95% CI')
+    plt.xlabel('Genetic relatedness coefficient')
+    plt.ylabel('Squared difference in trait values')
+    plt.title('Haseman-Elston Regression')
+    plt.show()
