8de58ec74a1679cbd978223c3054c431a3ab86b1
Nuno Carvalho <47678528+nunorgcarvalho@users.noreply.github.com>
2026-01-26 17:32:41 -0500

    paths are now categorized into relationship types

--- code/popstatgen/popsim.py
+++ code/popstatgen/popsim.py
@@ -22,6 +22,7 @@ from scipy.linalg import block_diag
 import copy
 import inspect
 import warnings
+from collections import namedtuple
 
 class Population:
     '''
@@ -669,7 +670,7 @@ class Population:
             for key, value in pops[gen].ped.paths.items():
                 # need to shift indices based on generation
                 key = (key[0] + Ns_cumsum[gen], key[1] + Ns_cumsum[gen])
-                new_pop.ped.paths[key] = value
+                new_pop.ped.paths[key] = new_pop.ped.intern_path(value)
         # fills in off-diagonal blocks, by going through each adjacent diagonal at a time
         for gap in range(1, generations+1):
             for start_gen in range(generations+1 - gap):
@@ -1322,6 +1323,28 @@ class SuperPopulation:
 # data types
 PedPath = Tuple[int, ...] # stores chain of meioses up/down, modified from [Williams et al. 2025 Genetics]
 PedKey2 = Tuple[int,int] # stores pair of individual indices (i,j)
+PathSig = namedtuple('PathSig', ['ups', 'downs', 'up_first','up_last', 'up3s', 'down3s']) # signature of a path for hashing
+
+@dataclass
+class RelObj:
+    '''
+    Class for storing relationship object information.
+    Attributes:
+    '''
+    sigs: PathSig # signatures corresponding to this relationship, see extract_signatures()
+    degree: Optional[int] = None # degree of relatedness, e.g. 1 for first-degree relatives
+    possible_inbreeding: Optional[bool] = None # whether this relationship may involve inbreeding based on the path
+    coeff_of_relatedness: Optional[float] = None # expected coefficient of relatedness given degree
+    
+    # key properties of relationship
+    direction: Optional[str] = None # direction of relationship, e.g. 'ancestor', 'descendant'
+    full_half: Optional[str] = None # 'full' or 'half' relationship
+    parental_line: Optional[str] = None # 'maternal' or 'paternal' or 'both' relationship
+
+    # naming of relationship at different levels
+    relationship: Optional[str] = None # name of relationship, broad-level e.g. 'sibling'
+    relationship2: Optional[str] = None # name of relationship, specific-level e.g. 'full_sibling'
+    relationship3: Optional[str] = None # name of relationship, detailed-level e.g. 'maternal_full_sibling'
 
 class Pedigree:
     def __init__(self, N: int, par_idx: np.ndarray = None, par_Ped: 'Pedigree' = None):
@@ -1341,6 +1364,9 @@ class Pedigree:
 
         # maps PedPath to a canonical PedPath for storage efficiency
         self._paths: Dict[PedPath, PedPath] = {}
+        # maps PathSig to Relatinship Object for quick lookup
+        self._relobjs: Dict[PedPath, RelObj] = {}
+        self.rels: Dict[PedKey2, RelObj] = {} # pairwise relationship object dictionary
 
         # fills in paths with self-relationships (denoted by () )
         self.fill_paths_self()
@@ -1396,14 +1422,28 @@ class Pedigree:
                 closest_path_keys = [key]
                 continue
             # first check: path cannot be longer than closest path so far
-            if len(path) > len(closest_path):
+            if len(path) < len(closest_path):
+                closest_path = path
+                closest_path_keys = [key]
                 continue
+            elif len(path) > len(closest_path):
+                continue
+            
             # second check: path cannot have less (+3) entries than closest path so far 
-            if path.count(3) < closest_path.count(3):
+            if path.count(3) > closest_path.count(3):
+                closest_path = path
+                closest_path_keys = [key]
+                continue
+            elif path.count(3) < closest_path.count(3):
                 continue
             # third check: path cannot have less (-1) entries than closest path so far
-            if path.count(-1) < closest_path.count(-1):
+            if path.count(-1) > closest_path.count(-1):
+                closest_path = path
+                closest_path_keys = [key]
                 continue
+            elif path.count(-1) < closest_path.count(-1):
+                continue
+
             # if all these checks are passed, we set this as the closest path
             # if this path is identical to the previously stored closest path, we append the key
             if path == closest_path:
@@ -1411,6 +1451,23 @@ class Pedigree:
             else:
                 closest_path = path
                 closest_path_keys = [key]
+            
+            # # first check: path cannot be longer than closest path so far
+            # if len(path) > len(closest_path):
+            #     continue
+            # # second check: path cannot have less (+3) entries than closest path so far 
+            # if path.count(3) < closest_path.count(3):
+            #     continue
+            # # third check: path cannot have less (-1) entries than closest path so far
+            # if path.count(-1) < closest_path.count(-1):
+            #     continue
+            # # if all these checks are passed, we set this as the closest path
+            # # if this path is identical to the previously stored closest path, we append the key
+            # if path == closest_path:
+            #     closest_path_keys.append(key)
+            # else:
+            #     closest_path = path
+            #     closest_path_keys = [key]
             
         return closest_path, closest_path_keys
     
@@ -1478,35 +1535,6 @@ class Pedigree:
         # handles self-relationships
         self.fill_paths_self()
 
-    def determine_degs(self):
-        '''
-        Fills in the degs dictionary with the degree of relatedness between every related individual in the current population. Uses the paths attribute to determine degree of relatedness. Only entries i>=j are explicitly computed, since the reverse relationships are automatically filled in.
-        
-        NOTE: For certain inbred pedigrees, the paths cannot unambiguously distinguish them, and thus the degree of relatedness may be incorrect. An example is double-first cousins vs two individuals whose 4 parents are all full-siblings with each other. Both have a path of [+3 +3 -3 -3], but the former is degree 2, and the latter is degree 1. This method will classify both as degree 2. Such cases are rare in practice, however. An attribute `errorprone_paths` is added to the Pedigree object that stores all paths that may be error-prone due to this issue. However, even this  will not catch all error-prone paths; e.g. the full-sibling parents example will not be caught, as it's indistinguishable from double-first cousins, who are truly degree 2. 
-        '''
-        errorprone_paths = {}
-        for (i,j), path in self.paths.items():
-            if i > j:
-                continue # only computes for i>=j
-            # degree is defined by counting up each entry in the path according to:
-            # +1, +2, -2, -1 -> +1 degree
-            # +3, -3 -> +0.5 degrees
-            # because of symmetry, the degree of non-inbred paths should always end up as an integer
-            # when this doesn't happen
-            count_3 = path.count(3)
-            count_neg3 = path.count(-3)
-            if count_3 != count_neg3:
-                # note that even this check will NOT catch all error-prone paths, but it will catch some of them (e.g. the full-sibling parents example will not be caught, as it's indistinguishable from double-first cousins, who are truly degree 2)
-                errorprone_paths[(i,j)] = path
-            deg_ij = len(path) - 0.5*(count_3 + count_neg3)
-            deg_ij = int(deg_ij) # converts to integer (floors it) 
-            self.degs[(i,j)] = deg_ij
-            self.degs[(j,i)] = deg_ij # symmetric
-        
-        self.errorprone_paths = errorprone_paths # stores paths that may be error-prone due to symmetry issues
-
-        print(f'WARNING: Out of {len(self.paths)} total paths, at least {len(self.errorprone_paths)} paths may have innaccurate degrees of relatedness due to inbred pedigree structure. These are stored in the errorprone_paths attribute of the Pedigree object.')
-
     def summarize_degrees(self) -> Dict[int, int]:
         '''
         Summarizes the degrees of relatedness in pedigree by counting the number of pairs for each degree. Note that pairs are counted twice (i,j) and (j,i).
@@ -1522,3 +1550,185 @@ class Pedigree:
         # sorts keys
         deg_summary = dict(sorted(deg_summary.items()))
         return deg_summary
+
+    def extract_signatures(self, path: PedPath) -> PathSig:
+        '''
+        Extracts a signature from a PedPath for hashing purposes. The signature consists of the number of ups, number of downs, whether the first step is up or down, and whether the last step is up or down.
+        Parameters:
+            path (PedPath): The path to extract the signature from.
+        Returns:
+            PathSig: The signature of the path.
+        '''
+        positives = [step for step in path if step > 0]
+        negatives = [step for step in path if step < 0]
+        # extract number of up meioses (i.e. positive entries)
+        ups = len(positives)
+        # extract number of down meioses (i.e. negative entries)
+        downs = len(negatives)
+        # extract the value of the first positive step
+        up_first = positives[0] if ups > 0 else -9
+        # extract the value of the last positive step
+        up_last = positives[-1] if ups > 0 else -9 
+
+        # extract number of +3s and -3s
+        up3s = positives.count(3)
+        down3s = negatives.count(-3)
+
+        # returns PathSig namedtuple
+        return PathSig(ups=ups, downs=downs, up_first=up_first, up_last=up_last, up3s=up3s, down3s=down3s)
+    
+    def path_to_relationship(self, path: PedPath) -> RelObj:
+        '''
+        Takes a PedPath object and converts it to a relationship object (RelObj) based on predefined signature rules.
+        Parameters:
+            path (PedPath): The path to convert to a relationship object.
+        Returns:
+            RelObj: The relationship object corresponding to the path.
+        '''
+        # extracts signatures of path
+        sigs = self.extract_signatures(path)
+        # begins building relationship object
+        rel_obj = RelObj(sigs=sigs)
+
+        # loops through each attribute of a relationship (i.e. general descriptors)
+        for rel_attribute in relative_types.keys():
+            # loops through each type within that attribute
+            for rel_type, rel_info in relative_types[rel_attribute].items():
+                sigs_match = True
+                # loops through each signature constraint of that type
+                for sig_key, sig_value in rel_info['sigs'].items():
+                    attr_value = getattr(sigs, sig_key)
+                    # if the ruleset only has a single value, it's converted to a list
+                    if isinstance(sig_value, int):
+                        sig_value = [sig_value]
+                    # if list, treated like set of values
+                    if isinstance(sig_value, list):
+                        if attr_value not in sig_value:
+                            sigs_match = False
+                            break
+                    # if tuple, is treated like a range
+                    if isinstance(sig_value, tuple) and len(sig_value) == 2:
+                        if attr_value < sig_value[0] or attr_value > sig_value[1]:
+                            sigs_match = False
+                            break
+                # assigns relationship attribute if signatures match
+                if sigs_match:
+                    setattr(rel_obj, rel_attribute, rel_type)
+                    break # each attribute can only have one type
+        
+        # builds detailed relationship names (e.g. full vs half and maternal vs paternal)
+        # by default, relationship2 is the same as relationship
+        rel_obj.relationship2 = rel_obj.relationship
+        # determines if full_half is relevant
+        fh = True
+        if 'fh' in relative_types['relationship'][rel_obj.relationship]:
+            fh = relative_types['relationship'][rel_obj.relationship]['fh']
+        # if full_half is relevant, adds it to relationship2
+        if fh:
+            rel_obj.relationship2 = rel_obj.full_half + '_' + rel_obj.relationship
+        # by default, relationship3 is the same as relationship2
+        rel_obj.relationship3 = rel_obj.relationship2
+        # if relationship is either half or parental, adds parental_line to relationship3
+        if rel_obj.parental_line == 'maternal' or rel_obj.parental_line == 'paternal':
+            rel_obj.relationship3 = rel_obj.parental_line + '_' + rel_obj.relationship2
+
+        # computes degree
+
+        # when up3s and down3s are unequal, inbreeding may be involved, and degree may be inaccurate
+        if sigs.up3s != sigs.down3s:
+            rel_obj.possible_inbreeding = True
+        else:
+            rel_obj.possible_inbreeding = False
+        # NOTE: For certain inbred pedigrees, the paths cannot unambiguously distinguish them, and thus the degree of relatedness may be incorrect. An example is double-first cousins vs two individuals whose 4 parents are all full-siblings with each other. Both have a path of [+3 +3 -3 -3], but the former is degree 2, and the latter is degree 1. This method will classify both as degree 2, and will not flag it as possible inbreeding. Such cases are rare in practice, however.
+        rel_obj.degree = int( sigs.ups + sigs.downs - (sigs.up3s + sigs.down3s)*0.5 )
+
+        # stores expected genome-wide coefficient of relatedness
+        rel_obj.coeff_of_relatedness = 2**(-rel_obj.degree)
+
+        return rel_obj
+    
+    def assign_relationships(self):
+        '''
+        Fills in the 'rels' dictionary with the relationship objects between every related individual in the current population. Uses the paths attribute to determine relationships, which means the 'paths' dictionary must be filled in first. See construct_paths().
+        '''
+        self._relobjs = {path: self.path_to_relationship(path) for path in self._paths.keys()}
+        self.rels = {key: self._relobjs[path] for key, path in self.paths.items()}
+
+    def count_relationships(self, attribute = 'relationship', i: int = None) -> Dict[str, int]:
+        '''
+        Counts the number of each type of relationship, or any other specified attribute of the RelObj, in the 'rels' dictionary.
+        Parameters:
+            attribute (str): The attribute of the RelObj to summarize. Default is 'relationship'.
+            i (int): If specified, only counts relationships involving individual i. Default is None, meaning all relationships are counted.
+        Returns:
+            rel_summary (Dict): Dictionary summarizing the number of each type of relationship.
+        '''
+        rel_summary: Dict[str, int] = {}
+        for rel_key, rel_obj in self.rels.items():
+            # if i is specified, only counts relationships involving individual i
+            if i is not None and rel_key[0] != i and rel_key[1] != i:
+                continue
+            rel_type = getattr(rel_obj, attribute)
+            if rel_type in rel_summary:
+                rel_summary[rel_type] += 1
+            else:
+                rel_summary[rel_type] = 1
+        # sorts keys
+        rel_summary = dict(sorted(rel_summary.items()))
+        return rel_summary
+
+
+
+# Note: a path from A-->B is classified as the relationship of B with respect to A. So, e.g., if A-->B is [+1], then that path is classified as 'parent', because B is the parent of A.
+# within each attribute, the keys are:
+# - sigs: dictionary of signature constraints to match. If value is a single integer, it is treated as an exact match. If value is a list, it is treated as a set of possible values. If value is a tuple of length 2, it is treated as a range (inclusive). A missing signature attribute means it is not constrained.
+# - fh: Whether distinguishing full vs half relatives is relevant. Default (e.g. if missing) is True. Only applicable to 'relationship' attribute.
+relative_types = {
+    'direction' : { # direct ancestors vs descendants
+        'direct_ancestor':      { 'sigs': {'ups': (1,9999), 'downs': 0} },
+        'direct_descendant':    { 'sigs': {'ups': 0, 'downs': (1,9999)} },
+        'N/A':                  { 'sigs': {} }
+    },
+    'full_half' : { # full vs half relatives
+        'full': { 'sigs': {'up_last': 3 } },
+        'half': { 'sigs': {'up_last': (1,2)} },
+        'N/A':  { 'sigs': {'up_last':-9} }
+    },
+    'parental_line' : { # side of family
+        'maternal': { 'sigs': {'up_first': 1} },
+        'paternal': { 'sigs': {'up_first': 2} },
+        'both':     { 'sigs': {'up_first': 3} },
+        'N/A':      { 'sigs': {'up_first':-9} } # for self
+    },
+    'relationship' : { # broad relationship types
+        # deg 0
+        'self':         { 'sigs': {'ups': 0, 'downs': 0}, 'fh': False },
+        # deg 1
+        'parent':       { 'sigs': {'ups': 1, 'downs': 0}, 'fh': False },
+        'offspring':    { 'sigs': {'ups': 0, 'downs': 1}, 'fh': False },
+        'sibling':      { 'sigs': {'ups': 1, 'downs': 1} },
+        # deg 2
+        'uncle/aunt':   { 'sigs': {'ups': 2, 'downs': 1} },
+        'nephew/niece': { 'sigs': {'ups': 1, 'downs': 2} },
+        'grandparent':  { 'sigs': {'ups': 2, 'downs': 0}, 'fh': False },
+        'grandchild':   { 'sigs': {'ups': 0, 'downs': 2}, 'fh': False },
+        'double_1cousin': { 'sigs': {'ups': 2, 'downs': 2, 'up_first': 3, 'up_last': 3} },
+        # deg 3
+        '1cousin': { 'sigs': {'ups': 2, 'downs': 2} },
+        'g1_grandparent': { 'sigs': {'ups': 3, 'downs': 0}, 'fh': False },
+        'g1_grandchild': { 'sigs': {'ups': 0, 'downs': 3}, 'fh': False },
+        'g1_uncle/aunt': { 'sigs': {'ups': 3, 'downs': 1} },
+        'g1_nephew/niece': { 'sigs': {'ups': 1, 'downs': 3} },
+        # deg 4
+        '1cousin_1removed': { 'sigs': {'ups': 2, 'downs': 3} },
+        'parent_1cousin': { 'sigs': {'ups': 3, 'downs': 2} },
+        'g2_grandparent': { 'sigs': {'ups': 4, 'downs': 0}, 'fh': False },
+        'g2_grandchild': { 'sigs': {'ups': 0, 'downs': 4}, 'fh': False },
+        'g2_uncle/aunt': { 'sigs': {'ups': 4, 'downs': 1} },
+        'g2_nephew/niece': { 'sigs': {'ups': 1, 'downs': 4} },
+        # deg 5
+        '2cousin': { 'sigs': {'ups': 3, 'downs': 3} },
+        # Catch-all, always leave at the end
+        'unspecified': {'sigs': {} }
+    }
+}
